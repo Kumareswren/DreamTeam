62a3a77cc27a7e07d09c6fe2b805702d2badb59e
loggersixnine <125236512+loggersixnine@users.noreply.github.com>
2024-03-21 00:52:30 +0800

    Update tutorDashboard.php

--- tutorDashboard.php
+++ tutorDashboard.php
@@ -546,7 +546,16 @@ a.createTutorBlog:hover {
     };
 </script>
 
-
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            var swiper = new Swiper(".swiper-container", {
+                slidesPerView: 3,
+                spaceBetween: 10,
+                loop: true,
+            });
+        });
+    </script>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
